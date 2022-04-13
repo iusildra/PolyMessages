d49df32a2cf5fb8855236a65b60227ce8103a34e
Agnese <neutron34980@gmail.com>
2022-04-13 12:58:36 +0200

    template, encore des erreurs
    Co-authored-by: Lucas Nouguier <iusildra@users.noreply.github.com>

--- src/client.c
+++ src/client.c
@@ -4,6 +4,73 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+
+int i = 2;
+
+//0 : int dS, 1 : struct sockaddr_in aS, 2 : socklen_t lgA
+void* sendMsg(void* values){
+  do
+  {
+    int maxSize = 128;
+    char *fin = "fin";
+    size_t size;
+
+    char *msg = malloc(sizeof(char)*maxSize);
+    printf("Message -> ");
+    fgets(msg, maxSize, stdin);
+    size = strlen(msg)+1;
+
+    if (strcmp(msg, fin) == 0)
+    {
+      // free(msg);dS, aS, lgA)
+      // break;
+    }
+
+    if(sendto((int)values[0], &size, sizeof(size_t), 0, (struct sockaddr *)&values[1], (socklen_t)values[2]) == -1) {
+      perror("error sendto");
+      exit(1);
+    }
+
+    if(sendto((int)values[0], msg, size, 0, (struct sockaddr *)&values[1],(socklen_t)values[2]) == -1) {
+      perror("error sendto");
+      exit(1);
+    }
+
+    free(msg);
+  } while (1);
+}
+
+
+void* receiveMsg(void* dS){
+  do
+  {
+    char *fin = "fin";
+    size_t size;
+
+    if (recv(dS, &size, sizeof(size_t), 0) == -1){
+      perror("error recv server");
+      exit(1);
+    }
+
+    char *msg = malloc(sizeof(char)*size);
+
+    if (recv(dS, msg, sizeof(char)*size, 0) == -1){
+      perror("error recv");
+      exit(1);
+    }
+    printf("Client 2 -> %s", msg);
+
+    if (strcmp(msg, fin) == 0)
+    {
+      // free(msg);
+      // break;
+    }
+
+    free(msg);
+  } while (1);
+  pthread_exit(&i);
+}
 
 int main(int argc, char *argv[]) {
   printf("Début programme\n");
@@ -23,44 +90,12 @@ int main(int argc, char *argv[]) {
   connect(dS, (struct sockaddr *) &aS, lgA) ;
   printf("Socket Connecté\n");
 
-  int nbOctet = 0;
-  int maxSize = 128;
-  char *fin = "fin";
-  size_t size;
-  do
-  {
-    char *msg = malloc(sizeof(char)*maxSize);
-    printf("Message -> ");
-    fgets(msg, maxSize, stdin);
-    size = strlen(msg)+1;
+  pthread_t idSend;
+  void* values = [dS, aS, lgA] ;
+  pthread_create(&idSend, NULL, sendMsg, values);
 
-    if (strcmp(msg, fin) == 0)
-    {
-      // break;
-    }
-
-    if(sendto(dS, &size, sizeof(size_t), 0, (struct sockaddr *)&aS, lgA) == -1) {
-      perror("error sendto");
-      exit(1);
-    }
-
-    if(sendto(dS, msg, size, 0, (struct sockaddr *)&aS, lgA) == -1) {
-      perror("error sendto");
-      exit(1);
-    }
-
-    if (recv(dS, &size, sizeof(size_t), 0) == -1){
-      perror("error recv server");
-      exit(1);
-    }
-
-    if (recv(dS, msg, sizeof(char)*size, 0) == -1){
-      perror("error recv");
-      exit(1);
-    }
-    printf("Client 2 -> %s", msg);
-    free(msg);
-  } while (1);
+  pthread_t idRcv;
+  pthread_create(&idRcv, NULL, receiveMsg, &dS);
 
   if (close(dS) == -1)
   {
