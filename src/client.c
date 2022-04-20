da8d203bf33812944944637f53ec4d00f3b3ad56
Lucas Nouguier <lucas.nouguier@protonmail.com>
2022-04-20 11:39:16 +0200

    Multi-client handling

--- src/client.c
+++ src/client.c
@@ -6,30 +6,37 @@
 #include <unistd.h>
 #include <pthread.h>
 #include "client.h"
+#define usernameMaxLength 20
 
 int NB_THREADS = 2;
 
-struct values {
+struct values
+{
   int socket;
   struct sockaddr_in sockaddr;
   socklen_t socklen;
-}values;
+  char *username;
+} values;
 int i = 2;
 
-//0 : int dS, 1 : struct sockaddr_in aS, 2 : socklen_t lgA
-//sendThread : allows a client to send a message to the server
-void* sendMsg(void* val){
-  struct values *param = (struct values*)val;
+// 0 : int dS, 1 : struct sockaddr_in aS, 2 : socklen_t lgA
+// sendThread : allows a client to send a message to the server
+void *sendMsg(void *val)
+{
+  struct values *param = (struct values *)val;
   do
   {
-    
-    int maxSize = 1024;
-    char *fin = "fin";
     size_t size;
-    char *msg = malloc(sizeof(char)*maxSize);
-    printf("Message -> ");
+    char *fin = "fin";
+    int maxSize = 1024;
+    char msg[maxSize];
+    char *fullMsg = malloc(sizeof(char) * (maxSize + strlen(param->username)));
+
     fgets(msg, maxSize, stdin);
-    size = strlen(msg)+1;
+    strcpy(fullMsg, param->username);
+    strcat(fullMsg, " -> ");
+    strcat(fullMsg, msg);
+    size = strlen(fullMsg) + 1;
 
     if (strcmp(msg, fin) == 0)
     {
@@ -37,44 +44,49 @@ void* sendMsg(void* val){
       // break;
     }
 
-    //Send the size of the message
-    if(sendto(param->socket, &size, sizeof(size_t), 0, (struct sockaddr *)&(param->sockaddr), param->socklen) == -1){
+    // Send the size of the message
+    if (send(param->socket, &size, sizeof(size_t), 0) == -1)
+    {
       perror("error sendto");
       exit(1);
     }
 
-    //Send the message itself
-    if(sendto(param->socket, msg, size, 0, (struct sockaddr *)&param->sockaddr,param->socklen) == -1) {
+    // Send the message itself
+    if (send(param->socket, fullMsg, size, 0) == -1)
+    {
       perror("error sendto");
       exit(1);
     }
 
-    free(msg);
+    free(fullMsg);
   } while (1);
 }
 
-//recvThread : allows a client to receive a message
-void* receiveMsg(void* params){
-  struct values *parameters = (struct values*) params;
+// recvThread : allows a client to receive a message
+void *receiveMsg(void *params)
+{
+  struct values *parameters = (struct values *)params;
   do
   {
     char *fin = "fin";
     size_t size;
 
-    //Receive the size of the message
-    if (recv(parameters->socket, &size, sizeof(size_t), 0) == -1){
+    // Receive the size of the message
+    if (recv(parameters->socket, &size, sizeof(size_t), 0) == -1)
+    {
       perror("error recv server");
       exit(1);
     }
 
-    char *msg = malloc(sizeof(char)*size);
+    char *msg = malloc(sizeof(char) * size);
 
-    //Receive the message itself
-    if (recv(parameters->socket, msg, sizeof(char)*size, 0) == -1){
+    // Receive the message itself
+    if (recv(parameters->socket, msg, sizeof(char) * size, 0) == -1)
+    {
       perror("error recv");
       exit(1);
     }
-    printf("Client 2 -> %s", msg);
+    printf("%s", msg);
 
     if (strcmp(msg, fin) == 0)
     {
@@ -89,38 +101,45 @@ void* receiveMsg(void* params){
 
 int main(int argc, char *argv[])
 {
-	printf("Début programme\n");
-  //Socket initialization
-	int dS = socket(PF_INET, SOCK_STREAM, 0);
-	if (dS == -1)
-	{
-		perror("NO CONNECTION TO SERVER");
-		exit(1);
-	}
-	printf("Socket Créé\n");
+  printf("Début programme\n");
+  if (strlen(argv[3]) >= usernameMaxLength)
+  {
+    perror("Username too long (must be < 20)");
+    exit(1);
+  }
+
+  // Socket initialization
+  int dS = socket(PF_INET, SOCK_STREAM, 0);
+  if (dS == -1)
+  {
+    perror("NO CONNECTION TO SERVER");
+    exit(1);
+  }
+  printf("Socket Créé\n");
 
   struct sockaddr_in aS;
   aS.sin_family = AF_INET;
   inet_pton(AF_INET, argv[1], &(aS.sin_addr));
   aS.sin_port = htons(atoi(argv[2]));
   socklen_t lgA = sizeof(struct sockaddr_in);
-  connect(dS, (struct sockaddr *) &aS, lgA);
+  connect(dS, (struct sockaddr *)&aS, lgA);
   printf("Socket Connecté\n");
 
-  pthread_t sendThread ;
+  pthread_t sendThread;
   pthread_t recvThread;
 
-  //Values to pass to the sendThread
+  // Values to pass to the sendThread
   struct values val;
   val.socket = dS;
   val.sockaddr = aS;
   val.socklen = lgA;
+  val.username = argv[3];
 
-  //Create thread
-  pthread_create(&sendThread, NULL, sendMsg, (void*)&val);
-  pthread_create(&recvThread, NULL, receiveMsg, (void*)&val);
+  // Create thread
+  pthread_create(&sendThread, NULL, sendMsg, (void *)&val);
+  pthread_create(&recvThread, NULL, receiveMsg, (void *)&val);
 
-  //Wait for threads to finish
+  // Wait for threads to finish
   pthread_join(sendThread, NULL);
   pthread_join(recvThread, NULL);
 
--- src/serveur.c
+++ src/serveur.c
@@ -6,142 +6,111 @@
 #include <pthread.h>
 #include <unistd.h>
 #include "serveur.h"
-#define MAX_NB_CLIENTS 2
+#define MAX_NB_CLIENTS 10
 
 // Parameters needed to send/receive a message
-struct params
+struct parametres_struct
 {
-  struct parametres_struct
-  {
-    int socks[MAX_NB_CLIENTS];
-    int nbClients;
-    socklen_t lg;
-    struct sockaddr_in aC;
-  } connection;
+  int dS;
+  socklen_t lg;
+  int nbClients;
+  int socks[MAX_NB_CLIENTS];
+  struct sockaddr_in aC;
+};
+struct parametres_struct connection;
+
+struct clientParams
+{
   size_t size;
+  unsigned short position;
 };
 
 // Transfer Client 1's message to Client 2
-void *C1versC2(void *params)
+void *clientManagement(void *params)
 {
 
-  struct params *param = (struct params *)params;
+  struct clientParams *param = (struct clientParams *)params;
 
   while (1)
   {
+    printf("Client : %d\n", connection.socks[param->position]);
     // Receive the message's size
-    if (recv(param->connection.socks[0], &(param->size), sizeof(size_t), 0) == -1)
+    if (recv(connection.socks[param->position], &(param->size), sizeof(size_t), 0) == -1)
     {
       perror("error recv server");
       exit(1);
     }
+
     char *msg = malloc(sizeof(char) * (param->size));
 
-    // if (strcmp(msg, "fin") == 0){
-    //   shutdown(dS, 2);
-    //   shutdown(dS2, 2);
-    //   listen(dS, 7);
-    //   listen(dS2, 7);
-    // }
     // Receive the message itself
-    if (recv(param->connection.socks[0], msg, sizeof(char) * (param->size), 0) == -1)
+    if (recv(connection.socks[param->position], msg, sizeof(char) * (param->size), 0) == -1)
     {
       perror("error recv server");
       exit(1);
     }
 
     // Send the message's size
-    if (sendto(param->connection.socks[1], &(param->size), sizeof(size_t), 0, (struct sockaddr *)&(param->connection.aC), param->connection.lg) == -1)
+    for (int i = 0; i < connection.nbClients; i++)
     {
-      perror("error sendto server");
-      exit(1);
-    }
-    // Send the message itself
-    if (sendto(param->connection.socks[1], msg, param->size, 0, (struct sockaddr *)&(param->connection.aC), param->connection.lg) == -1)
-    {
-      perror("error sendto server");
-      exit(1);
+      printf("Send to %d\n", connection.socks[i]);
+      if (sendto(connection.socks[i], &(param->size), sizeof(size_t), 0, (struct sockaddr *)&(connection.aC), connection.lg) == -1)
+      {
+        perror("error sendto server");
+        exit(1);
+      }
+      // Send the message itself
+      if (sendto(connection.socks[i], msg, param->size, 0, (struct sockaddr *)&(connection.aC), connection.lg) == -1)
+      {
+        perror("error sendto server");
+        exit(1);
+      }
     }
     free(msg);
   }
+  free(params);
   pthread_exit(0);
 }
 
-// Transfer Client 2's message to Client 1
-void *C2versC1(void *params)
+void *userLogin()
 {
-
-  struct params *param = (struct params *)params;
-
-  while (1)
+  pthread_t thread[MAX_NB_CLIENTS];
+  do
   {
-    // Receive the message's size
-    if (recv(param->connection.socks[1], &(param->size), sizeof(size_t), 0) == -1)
-    {
-      perror("error recv");
-      exit(1);
-    }
+    struct clientParams* clientParams = malloc(sizeof(struct clientParams));
+    connection.socks[connection.nbClients] = accept(connection.dS, (struct sockaddr *)&connection.aC, &connection.lg);
+    clientParams->position = connection.nbClients;
+    printf("Client added ? %d\n", pthread_create(&thread[clientParams->position], NULL, clientManagement, (void *)clientParams));
+    connection.nbClients++;
+  } while (connection.nbClients < MAX_NB_CLIENTS);
 
-    char *msg = malloc(sizeof(char) * (param->size));
-    // Receive the message itself
-    if (recv(param->connection.socks[1], msg, sizeof(char) * (param->size), 0) == -1)
-    {
-      perror("error recv server");
-      exit(1);
-    }
-
-    // Send the message's size
-    if (sendto(param->connection.socks[0], &(param->size), sizeof(size_t), 0, (struct sockaddr *)&(param->connection.aC), param->connection.lg) == -1)
-    {
-      perror("error sendto server");
-      exit(1);
-    }
-    // Send the message itself
-    if (sendto(param->connection.socks[0], msg, param->size, 0, (struct sockaddr *)&(param->connection.aC), param->connection.lg) == -1)
-    {
-      perror("error sendto server");
-      exit(1);
-    }
-    free(msg);
+  for (int i = 0; i < MAX_NB_CLIENTS; i++)
+  {
+    pthread_join(thread[i], NULL);
   }
-  pthread_exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-
   printf("Début programme\n");
-  pthread_t thread[MAX_NB_CLIENTS];
 
-  struct params param;
-
-  int dS = socket(PF_INET, SOCK_STREAM, 0);
+  connection.dS = socket(PF_INET, SOCK_STREAM, 0);
   printf("Socket Créé\n");
 
   struct sockaddr_in ad;
   ad.sin_family = AF_INET;
   ad.sin_addr.s_addr = INADDR_ANY;
   ad.sin_port = htons(atoi(argv[1]));
-  bind(dS, (struct sockaddr *)&ad, sizeof(ad));
+  bind(connection.dS, (struct sockaddr *)&ad, sizeof(ad));
   printf("Socket Nommé\n");
 
-  listen(dS, 7);
-  printf("Mode écoute client 1\n");
-
+  listen(connection.dS, 7);
+  printf("Mode écoute client\n");
   struct sockaddr_in aC;
-  param.connection.aC = aC;
-  param.connection.lg = sizeof(struct sockaddr_in);
-  param.connection.socks[0] = accept(dS, (struct sockaddr *)&param.connection.aC, &param.connection.lg);
-  printf("Client 1 Connecté\n");
+  connection.aC = aC;
+  connection.lg = sizeof(struct sockaddr_in);
 
-  param.connection.socks[1] = accept(dS, (struct sockaddr *)&param.connection.aC, &param.connection.lg);
-  printf("Client 2 Connecté\n");
-
-  pthread_create(&thread[0], NULL, C1versC2, (void *)&param);
-  pthread_create(&thread[1], NULL, C2versC1, (void *)&param);
-
-  pthread_join(thread[0], NULL);
-  pthread_join(thread[1], NULL);
+  userLogin();
 
   // shutdown(dS2C, 2);
   // shutdown(dSC, 2);
